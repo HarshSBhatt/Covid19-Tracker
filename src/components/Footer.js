8ab0a8fb2d9fa6311c9e4702e1e447fe2a338ebf
HarshSBhatt <harsh@logicwind.com>
2020-04-01 10:27:50 +0530

    Done Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons'
 function Footer() {
     return (
         <div className='footer'>
+            <p>Source : Johns Hopkins University, MOHFW & Crowd sourced with verification</p>
             <h3>Made with{' '}{<FontAwesomeIcon icon={faHeart} />}{' '}in India</h3>
         </div>
     )
