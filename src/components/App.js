47dc2205e2fe7b4eab916907ecae8d4facaf7951
HarshSBhatt <harsh@logicwind.com>
2020-04-05 13:17:38 +0530

    ..

--- src/components/App.js
+++ src/components/App.js
@@ -11,11 +11,11 @@ import News from './news/News';
 import FAQ from './FAQ';
 
 function App() {
-  // useEffect(() => {
-  //   window.scroll(0, 0)
-  //   ReactGA.initialize('UA-162207556-1');
-  //   ReactGA.pageview(window.location.pathname + window.location.search);
-  // }, [])
+  useEffect(() => {
+    window.scroll(0, 0)
+    ReactGA.initialize('UA-162207556-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
   return (
     <Router>
       <div className="App">
